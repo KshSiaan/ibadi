adf7359a277c09d4127cf694e62cfc7c9772290f
mdnazmulhasanniloy <mdnazmulhasanniloy323@gmail.com>
2026-08-15 22:22:20 -0700

    some change

--- src/lib/firebase-auth.ts
+++
@@ -1,5 +0,0 @@
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
-import { firebaseApp } from "./config";
-
-export const firebaseAuth = getAuth(firebaseApp);
-export const googleProvider = new GoogleAuthProvider();
